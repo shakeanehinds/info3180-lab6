6e1aee9e9d0bd6cb23090febb648bda820445d73
shakeanehinds <44011884+shakeanehinds@users.noreply.github.com>
2019-03-26 00:20:11 -0500

    completed all routes and pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from newsapi import NewsApiClient
+
+newsapi = NewsApiClient(api_key='9b0fb733452547c38680f08afc5b853b')
 
 app = Flask(__name__)
 from app import views
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,6 +1,6 @@
 /* Add your Application JavaScript */
 Vue.component('app-header', {
-    template: `
+  template: `
         <header>
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
               <a class="navbar-brand" href="#">VueJS App</a>
@@ -21,29 +21,108 @@ Vue.component('app-header', {
             </nav>
         </header>    
     `,
-    data: function() {}
+  data: function () { }
 });
 
 Vue.component('app-footer', {
-    template: `
+  template: `
         <footer>
             <div class="container">
                 <p>Copyright &copy {{ year }} Flask Inc.</p>
             </div>
         </footer>
     `,
-    data: function() {
-        return {
-            year: (new Date).getFullYear()
-        }
+  data: function () {
+    return {
+      year: (new Date).getFullYear()
     }
+  }
 })
 
+const NewsList = Vue.component('news-list', {
 
-let app = new Vue({
-    el: '#app',
-    data: {
-        welcome: 'Hello World! Welcome to VueJS'
+  template: `
+  <div>
+    <h2 style="padding: 20px;"> News </h2>
+    <div class="row">
+      <div v-for="article in articles" class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <img class="card-img-top" :src="article.urlToImage"/>
+            <p class="card-title">{{ article.title}}</p>
+            <p class="card-text">{{ article.description}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="form-inline d-flex justify-content-center" style="padding: 20px;">
+      <div class="form-group mx-sm-3 mb-2">
+        <label class="sr-only" for="search">Search</label>
+        <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+        <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+      </div>
+    </div>
+  </div>
+
+
+  `,
+  created: function () {
+    let self = this;
+    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=putkeyhere')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        self.articles = data.articles;
+      });
+  },
+  data: function () {
+    return {
+      articles: [],
+      searchTerm: '' 
     }
-});
+  },
+  methods: {
+    searchNews: function() {
+    let self = this;
+    fetch('https://newsapi.org/v2/everything?q='+ self.searchTerm + '&language=en&apiKey=putkeyhere')
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(data) {
+    console.log(data);
+    self.articles = data.articles;
+    });
+    }
+    } 
+})
+
+const Home = Vue.component('home', {
+  template: `
+  <div class="home">
+    <img src="/static/images/logo.png" alt="VueJS Logo">
+    <h1>{{ welcome }}</h1>
+  </div>
+  `,
+  data: function() {
+  return {
+  welcome: 'Hello World! Welcome to VueJS'
+  }
+  }
+ });
+
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+  { path: '/', component: Home },
+  { path: '/news', component: NewsList }
+  ]
+ });
+
+ const app = new Vue({
+  el: '#app',
+  router
+ })
 
